ab4e07afc4dde2646b711738f92fa3467f1485ea
Viet Pham <pxviet1997@gmail.com>
2020-06-30 14:50:18 +1000

    Initial Commit

---
+++ Restaurant-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant-iOS
+//
+//  Created by Viet Pham on 30/6/20.
+//  Copyright © 2020 Viet Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
