f5b861c8050573512883fca29e673e1227340939
Viet Pham <pxviet1997@gmail.com>
2020-07-01 11:24:12 +1000

    Add MenuView and MenuViewController

---
+++ Restaurant-iOS/Controller/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  Restaurant-iOS
+//
+//  Created by Viet Pham on 1/7/20.
+//  Copyright © 2020 Viet Pham. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
